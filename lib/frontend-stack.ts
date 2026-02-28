594cd4ffbe8dfb22eff38e3781ff5fc471880ad7
suzunari900261 <uz7184@gmail.com>
2026-02-28 20:50:52 +0900

    update

--- lib/frontend-stack.ts
+++ lib/frontend-stack.ts
@@ -3,6 +3,7 @@ import { CfnOutput, CfnParameter, Stack, StackProps, Tags } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 import { S3BucketConstruct } from "./constructs/s3_bucket";
+import { CloudFrontConstruct} from "./constructs/cloudfront";
 
 export class FrontendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -43,8 +44,34 @@ export class FrontendStack extends Stack {
       environment.valueAsString,
     ]);
 
+    //S3バケット作成
     const s3Construct = new S3BucketConstruct(this, "S3frontConstruct", {
       bucketName: bucketNameWithEnv,
+    });
+
+    //CloudFront作成
+    const cloudfrontConstruct = new CloudFrontConstruct(
+      this,
+      "CloudFrontConstruct", 
+    {
+      originBucket: s3Construct.bucket,
+      isSpa: true,
+      defaultRootObject: "index.html",
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_200, 
+    }
+  );
+
+    const cloudfrontDistributionArn = cdk.Stack.of(this).formatArn({
+      service: "cloudfront",
+      resource: "distribution",
+      resourceName: cloudfrontConstruct.distribution.distributionId,
+      region: "",
+      account: cdk.Stack.of(this).account,
+    });
+
+    //S3バケットポリシー作成
+    s3Construct.grantReadFromCloudFront({
+      cloudfrontDistributionArn,
     });
 
     // ------------------------------------------------------------
@@ -52,6 +79,18 @@ export class FrontendStack extends Stack {
     // ------------------------------------------------------------
     new CfnOutput(this, "S3BucketNameOutput", {
       value: s3Construct.bucket.bucketName,
+    });
+
+    new CfnOutput(this, "CloudFrontDistributionId", {
+      value: cloudFrontConstruct.distribution.distributionId,
+    });
+
+    new CfnOutput(this, "CloudFrontDomainName", {
+      value: cloudFrontConstruct.distribution.distributionDomainName,
+    });
+
+    new CfnOutput(this, "CloudFrontUrl", {
+      value: `https://${cloudFrontConstruct.distribution.distributionDomainName}`,
     });
   }
 }
--- test/c_fn_template_builder.test.ts
+++ test/c_fn_template_builder.test.ts
@@ -1,17 +1,105 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import { FrontendStack } from '../lib/frontend-stack';
+import * as cdk from "aws-cdk-lib";
+import { Match, Template } from "aws-cdk-lib/assertions";
+import { FrontendStack } from "../lib/frontend-stack";
 
-test('S3 Bucket is created', () => {
-  const app = new cdk.App();
+describe("FrontendStack (S3 + CloudFront OAC)", () => {
+  const synth = () => {
+    const app = new cdk.App();
 
-  const stack = new FrontendStack(app, 'TestStack', {
-    env: {
-      region: 'ap-northeast-1', // リージョンのみ指定
-    },
+    const stack = new FrontendStack(app, "TestStack", {
+      env: { region: "ap-northeast-1" },
+    });
+
+    return Template.fromStack(stack);
+  };
+
+  test("S3 Bucket is created", () => {
+    const template = synth();
+    template.resourceCountIs("AWS::S3::Bucket", 1);
   });
 
-  const template = Template.fromStack(stack);
+  test("S3 Bucket has private + secure settings", () => {
+    const template = synth();
 
-  template.resourceCountIs('AWS::S3::Bucket', 1);
+    template.hasResourceProperties("AWS::S3::Bucket", {
+      // Block public access
+      PublicAccessBlockConfiguration: {
+        BlockPublicAcls: true,
+        BlockPublicPolicy: true,
+        IgnorePublicAcls: true,
+        RestrictPublicBuckets: true,
+      },
+
+      // SSE-S3 encryption (S3_MANAGED)
+      BucketEncryption: {
+        ServerSideEncryptionConfiguration: Match.arrayWith([
+          Match.objectLike({
+            ServerSideEncryptionByDefault: { SSEAlgorithm: "AES256" },
+          }),
+        ]),
+      },
+
+      // ObjectOwnership: BUCKET_OWNER_ENFORCED
+      OwnershipControls: {
+        Rules: Match.arrayWith([
+          Match.objectLike({ ObjectOwnership: "BucketOwnerEnforced" }),
+        ]),
+      },
+    });
+  });
+
+  test("BucketPolicy enforces SSL and allows CloudFront distribution read (OAC)", () => {
+    const template = synth();
+
+    // BucketPolicy 自体が作られていること（enforceSSL や addToResourcePolicy により生成される）
+    template.resourceCountIs("AWS::S3::BucketPolicy", 1);
+
+    template.hasResourceProperties("AWS::S3::BucketPolicy", {
+      PolicyDocument: {
+        Statement: Match.arrayWith([
+          // enforceSSL: true により HTTP を拒否する Deny が入ることが多い
+          Match.objectLike({
+            Effect: "Deny",
+            Action: "s3:*",
+            Condition: Match.objectLike({
+              Bool: Match.objectLike({
+                "aws:SecureTransport": "false",
+              }),
+            }),
+          }),
+
+          // grantReadFromCloudFront() で追加した Allow
+          Match.objectLike({
+            Sid: "AllowCloudFrontServicePrincipalReadOnly",
+            Effect: "Allow",
+            Action: "s3:GetObject",
+            Principal: { Service: "cloudfront.amazonaws.com" },
+            Resource: Match.anyValue(), // arn:...:bucket/* になる（Join等）
+            Condition: {
+              StringEquals: {
+                "AWS:SourceArn": Match.anyValue(), // formatArn の結果（Join/Ref を含む）
+              },
+            },
+          }),
+        ]),
+      },
+    });
+  });
+
+  test("CloudFront Distribution and OAC are created", () => {
+    const template = synth();
+
+    template.resourceCountIs("AWS::CloudFront::Distribution", 1);
+
+    // OACを使っている場合、多くの構成で OriginAccessControl リソースが作られる
+    template.resourceCountIs("AWS::CloudFront::OriginAccessControl", 1);
+
+    // ついでに defaultRootObject など軽く確認（必要なら）
+    template.hasResourceProperties("AWS::CloudFront::Distribution", {
+      DistributionConfig: Match.objectLike({
+        DefaultRootObject: "index.html",
+        Enabled: true,
+      }),
+    });
+  });
 });
