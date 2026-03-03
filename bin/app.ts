91def2c725491a0a30b5dbc03629f30492c8c303
suzunari900261 <uz7184@gmail.com>
2026-03-03 23:20:32 +0900

    Update app.ts

--- bin/app.ts
+++ bin/app.ts
@@ -7,6 +7,12 @@ const app = new cdk.App();
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region  = process.env.CDK_DEFAULT_REGION;
 
+if (!account || !region) {
+  throw new Error(
+    `Missing env. account=${account ?? "undefined"}, region=${region ?? "undefined"}`
+  );
+}
+
 new FrontendStack(app, 'CFnTemplateBuilder-FrontendStack', {
   env: { account, region },
   crossRegionReferences: true,
