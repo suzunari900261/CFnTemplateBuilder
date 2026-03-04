944cefd7333796453eb869ee1f4d22111b8d7dba
suzunari900261 <uz7184@gmail.com>
2026-03-04 18:38:05 +0900

    update

--- lib/constructs/lambda_edge.ts
+++ lib/constructs/lambda_edge.ts
@@ -2,24 +2,28 @@ import { Construct } from "constructs";
 import { aws_cloudfront as cloudfront } from "aws-cdk-lib";
 import { aws_lambda as lambda } from "aws-cdk-lib";
 import * as path from "path";
+import * as fs from "fs";
 
 export interface EdgeAuthConstructProps {
-  readonly cognitoDomain: string;
-  readonly userPoolClientId: string;
+  readonly cognitoDomain?: string;
+  readonly userPoolClientId?: string;
 }
 
 export class EdgeAuthConstruct extends Construct {
   public readonly edgeFn: cloudfront.experimental.EdgeFunction;
 
-  constructor(scope: Construct, id: string, props: EdgeAuthConstructProps) {
+  constructor(scope: Construct, id: string, props: EdgeAuthConstructProps = {}) {
     super(scope, id);
+
+    const cognitoDomain = props.cognitoDomain ?? "PLACEHOLDER";
+    const userPoolClientId = props.userPoolClientId ?? "PLACEHOLDER";
 
     const entry = path.join(process.cwd(), "lib", "lambda_edge", "edge-auth", "index.js");
     const template = fs.readFileSync(entry, "utf8");
 
     const inlined = template
-      .replace(/"__COGNITO_DOMAIN__"/g, JSON.stringify(props.cognitoDomain))
-      .replace(/"__CLIENT_ID__"/g, JSON.stringify(props.userPoolClientId));
+      .replace(/"__COGNITO_DOMAIN__"/g, JSON.stringify(cognitoDomain))
+      .replace(/"__CLIENT_ID__"/g, JSON.stringify(userPoolClientId));
 
     this.edgeFn = new cloudfront.experimental.EdgeFunction(this, "AuthEdgeFn", {
       runtime: lambda.Runtime.NODEJS_20_X,
