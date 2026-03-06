e8e51faee14450d6f25a83c8027ade263a911254
suzunari900261 <uz7184@gmail.com>
2026-03-06 23:46:27 +0900

    update

--- cdk/lib/constructs/cognito.ts
+++ cdk/lib/constructs/cognito.ts
@@ -1,60 +1,33 @@
 import { Construct } from "constructs";
-import { Stack } from "aws-cdk-lib";
-import {
-  aws_cognito as cognito,
-  Duration,
-  RemovalPolicy,
-} from "aws-cdk-lib";
-
-import {
-  AwsCustomResource,
-  AwsCustomResourcePolicy,
-  PhysicalResourceId,
-} from "aws-cdk-lib/custom-resources";
+import { Stack, RemovalPolicy, aws_cognito as cognito } from "aws-cdk-lib";
 
 export interface CognitoConstructProps {
   readonly callbackUrls: string[];
-  readonly logoutUrls: string[];
-  readonly cognitoDomainPrefix?: string;
-  readonly removalPolicy?: RemovalPolicy;
+  readonly logoutUrls?: string[];
+  readonly cognitoDomainPrefix: string;
 }
 
 export class CognitoConstruct extends Construct {
   public readonly userPool: cognito.UserPool;
   public readonly userPoolClient: cognito.UserPoolClient;
   public readonly issuerUrl: string;
-  public readonly hostedUiBaseUrl?: string;
+  public readonly hostedUiBaseUrl: string;
 
   constructor(scope: Construct, id: string, props: CognitoConstructProps) {
     super(scope, id);
 
-    const removalPolicy = props.removalPolicy ?? RemovalPolicy.RETAIN;
-
     this.userPool = new cognito.UserPool(this, "UserPool", {
-      userPoolName: "cfn-template-builder-users",
       selfSignUpEnabled: true,
       autoVerify: { email: true },
       standardAttributes: {
         email: { required: true, mutable: true },
       },
-      passwordPolicy: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireDigits: true,
-        requireSymbols: false,
-      },
-      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
-      removalPolicy,
+      removalPolicy: RemovalPolicy.RETAIN,
     });
 
     this.userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
       userPool: this.userPool,
       generateSecret: false,
-      authFlows: {
-        userSrp: true,
-        userPassword: true,
-      },
       oAuth: {
         flows: {
           authorizationCodeGrant: true,
@@ -65,23 +38,18 @@ export class CognitoConstruct extends Construct {
           cognito.OAuthScope.PROFILE,
         ],
         callbackUrls: props.callbackUrls,
-        logoutUrls: props.logoutUrls,
+        logoutUrls: props.logoutUrls ?? [],
       },
-      refreshTokenValidity: Duration.days(30),
-      accessTokenValidity: Duration.minutes(60),
-      idTokenValidity: Duration.minutes(60),
-      preventUserExistenceErrors: true,
     });
 
-    if (props.cognitoDomainPrefix) {
-      this.userPool.addDomain("HostedUiDomain", {
-        cognitoDomain: { domainPrefix: props.cognitoDomainPrefix },
-      });
-    }
-
-    this.issuerUrl = this.userPool.userPoolProviderUrl;
+    this.userPool.addDomain("HostedUiDomain", {
+      cognitoDomain: {
+        domainPrefix: props.cognitoDomainPrefix,
+      },
+    });
 
     const region = Stack.of(this).region;
     this.hostedUiBaseUrl = `https://${props.cognitoDomainPrefix}.auth.${region}.amazoncognito.com`;
+    this.issuerUrl = this.userPool.userPoolProviderUrl;
   }
 }
